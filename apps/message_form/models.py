5d04d4b0de91fc278405a7000fe1e215879c9468
zhfeng <zhfeng77@163.com>
2020-02-14 22:20:53 +0800

    方式2:将样式文件放在项目根目录下的static/css目录中，在settings.py中添加STATICFILES_DIRS完成对static路径的设置 - 4.建立与页面相关的数据库，并修改数据引擎为mysql，安装mysql的django驱动mysqlclient 建立和message表和与页面数据相对应的字段

--- apps/message_form/models.py
+++ apps/message_form/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    # 这里的verbose_name用于给字段一个可读性好的名字，如果不指定，则使用变量名，比如name
+    # prmary_key表示该字段是一个主键，如果不指定，会自动帮你生成一个id作为主键
+    name = models.CharField(max_length=20, verbose_name="姓名", primary_key=True)
+    email = models.EmailField(verbose_name="邮箱")
+    address = models.CharField(max_length=200, verbose_name="联系地址")
+    message = models.TextField(verbose_name="留言")
+
+    class Meta():   #Meta选项用于定义一些Django模型类的行为特性
+        verbose_name = "留言信息"           # verbose_name用于给你的模型类起一个可读性好的名字
+        verbose_name_plural = verbose_name # 这个选项是指定，模型的复数形式是什么，如果不指定Django会自动在模型名称后加一个’s’
+        db_table = "message"                # 定义数据库中的表名
--- readme.md
+++ readme.md
@@ -1,4 +1,7 @@
 - 1.完成django留言簿项目目录结构的构建
 - 2.配置静态目录：应用中的静态目录和全局静态目录，删除测试用的app2应用
-- 3.将message_form.html中的样式分离出来，保存到apps/message_form/static/css目录中命名为style.css
+- 3.方式1：将message_form.html中的样式分离出来，保存到apps/message_form/static/css目录中命名为style.css
 在message_form.html中添加对样式的引用<link rel="stylesheet" href="/static/css/style.css">
+方式2:将样式文件放在项目根目录下的static/css目录中，在settings.py中添加STATICFILES_DIRS完成对static路径的设置
+- 4.建立与页面相关的数据库，并修改数据引擎为mysql，安装mysql的django驱动mysqlclient
+建立和message表和与页面数据相对应的字段
